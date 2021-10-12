3a22693f5705a651e1809218e2ff0ce6dfb31355
troffe <troffe@DESKTOP-F2MMEOS>
2021-10-12 16:12:52 +0900

    last day staging   - input

---
+++ happyJava/src/happyJava/hihi.java
@@ -0,0 +1,12 @@
+package happyJava;
+
+public class hihi {
+	public static void main(String[] args) {
+		 System.out.println("  1231231123 hello");	
+	
+		 System.out.println("-------------------");
+		 System.out.println("staging에 올려보기");
+		 System.out.println("마지막날 수업자료 올려보기");
+	}
+
+}
